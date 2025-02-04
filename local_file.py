f109012b60371c07b18b866cde83a46cf0a7e102
krijankhatri <dilkshetri2020@gmail.com>
2025-02-04 18:08:35 +0545

    hawa2

--- local_file.py
+++ local_file.py
@@ -1,6 +1,4 @@
 print("This is a local repository.")
 print("New line is added to this local repository.")
-#addding conflictas
-#addding conflictas
-#addding conflictas
-#addding conflictas
+a = 10
+b = 20
